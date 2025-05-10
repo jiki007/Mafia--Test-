880c4c82b0fee95b4ccf3b5c2098b83d55f6e87c
jiki007 <asralkhanov@inha.edu>
2025-05-10 21:24:37 +0900

    /join command has been added

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -4,10 +4,29 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 #Defining the bot
 app = ApplicationBuilder().token("7490724483:AAEy3khPwbQ_U0BQgS65gcn15TptOgRz-Nc").build()
 
-#Start command
+#List of players
+player_list = {}
+MAX_PLAYERS = 10
+
+#Command Funcitions
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Welcome to Mafia Bot.")
 
-#Commands
+async def join(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    user_id = user.id
+    username = user.username or user.full_name
+
+    if user.id in player_list:
+        await update.message.reply_text(f"{username}, you already joined!")
+    elif len(player_list) >= MAX_PLAYERS:
+        await update.message.reply_text("Player limit reached. You can't join. Please wait until another game starts!")
+    else:
+        player_list[user_id] = username
+        await update.message.reply_text(f"{username} joined the game! ({len(player_list)}/{MAX_PLAYERS})")
+
+
+#Command Handlers
 app.add_handler(CommandHandler("start",start))
+app.add_handler(CommandHandler("join",join))
 
